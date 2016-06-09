3c91d32d9b64a948203d4a6626b1d03be41602b9
matmei99 <mmeier203@gmail.com>
2016-06-09 08:57:46 +0200

    some changes

--- LESS/src/view/Less.java
+++ LESS/src/view/Less.java
@@ -3,8 +3,9 @@
  * To change this template file, choose Tools | Templates
  * and open the template in the editor.
  */
-package less;
+package view;
 
+import com.phidgets.PhidgetException;
 import com.pi4j.io.gpio.GpioController;
 import com.pi4j.io.gpio.GpioFactory;
 import com.pi4j.io.gpio.GpioPinDigitalInput;
@@ -14,6 +15,7 @@ import com.pi4j.io.gpio.PinState;
 import com.pi4j.io.gpio.RaspiPin;
 import com.pi4j.io.gpio.event.GpioPinDigitalStateChangeEvent;
 import com.pi4j.io.gpio.event.GpioPinListenerDigital;
+import java.io.IOException;
 import java.util.logging.Level;
 import java.util.logging.Logger;
 import javafx.application.Application;
@@ -26,44 +28,70 @@ import javafx.stage.Stage;
  *
  * @author mario
  */
-public class Button extends Application  {
+public class Less extends Application {
 
     @Override
     public void start(Stage stage) throws Exception {
         Parent root = FXMLLoader.load(getClass().getResource("GUI.fxml"));
-        
+
         Scene scene = new Scene(root);
-        
+
         stage.setScene(scene);
         stage.setResizable(false);
         stage.show();
     }
+
     public static void main(String[] args) {
         launch(args);
 //        try {
 //            final GpioController gpio = GpioFactory.getInstance();
-            
+//
 //            final GpioPinDigitalInput myButton = gpio.provisionDigitalInputPin(RaspiPin.GPIO_02, PinPullResistance.PULL_DOWN);
-//            final GpioPinDigitalOutput led = gpio.provisionDigitalOutputPin(RaspiPin.GPIO_13, "MyLED", PinState.HIGH);
-//            final GpioPinDigitalOutput led2 = gpio.provisionDigitalOutputPin(RaspiPin.GPIO_14, "MyLED", PinState.LOW);
-//            FreeMode.runMode();
-            
-//            myButton.addListener((GpioPinListenerDigital)(GpioPinDigitalStateChangeEvent event) -> {
-//                if(led.isHigh()){
-//                    led.setState(PinState.LOW);
-//                    led2.setState(PinState.HIGH);
+//            final GpioPinDigitalOutput led = gpio.provisionDigitalOutputPin(RaspiPin.GPIO_13, "MyLED", PinState.LOW);
+//            Thread t;
+//            Thread t2;
+//            t = new Thread(new Runnable() {
+//
+//                @Override
+//                public void run() {
 //                    try {
-//                        QuestionMode.closeMode();
 //                        FreeMode.runMode();
 //                    } catch (Exception ex) {
 //                        Logger.getLogger(Button.class.getName()).log(Level.SEVERE, null, ex);
 //                    }
-//                }else{
-//                    led2.setState(PinState.LOW);
+//                }
+//            });
+//            t.setDaemon(true);
+//            t.start();
+//            t2 = new Thread(new Runnable() {
+//
+//                @Override
+//                public void run() {
+//                    try {
+//                        QuestionMode.runMode();
+//                    } catch (PhidgetException ex) {
+//                        Logger.getLogger(Button.class.getName()).log(Level.SEVERE, null, ex);
+//                    } catch (IOException ex) {
+//                        Logger.getLogger(Button.class.getName()).log(Level.SEVERE, null, ex);
+//                    }
+//                }
+//            });
+//            t2.setDaemon(true);
+//
+//            myButton.addListener((GpioPinListenerDigital) (GpioPinDigitalStateChangeEvent event) -> {
+//                if (led.isHigh()) {
+//                    led.setState(PinState.LOW);
+//                    try {
+//                        t2.stop();
+//                        t.start();
+//                    } catch (Exception ex) {
+//                        Logger.getLogger(Button.class.getName()).log(Level.SEVERE, null, ex);
+//                    }
+//                } else {
 //                    led.setState(PinState.HIGH);
 //                    try {
-//                        FreeMode.closeMode();
-//                        QuestionMode.runMode();
+//                        t.stop();
+//                        t2.start();
 //                    } catch (Exception ex) {
 //                        Logger.getLogger(Button.class.getName()).log(Level.SEVERE, null, ex);
 //                    }
